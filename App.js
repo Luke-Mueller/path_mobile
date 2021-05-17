42016a7455e951629a34d64672d0d11bf107b3e8
Luke Mueller <lukemueller@Lukes-MacBook-Pro.local>
2021-05-17 12:32:20 -0600

    adds app and package.json

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import Thunk from "redux-thunk";
+
+import AppNavigator from "./navigation/AppNavigator";
+import AuthReducer from "./store/reducers/auth";
+import ListReducer from './store/reducers/list';
+
+const rootReducer = combineReducers({
+  auth: AuthReducer,
+  list: ListReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(Thunk));
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
