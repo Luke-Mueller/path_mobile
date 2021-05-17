adf43159caa1b593cae21b5a9a709689942e13d8
Luke Mueller <lukemueller@Lukes-MacBook-Pro.local>
2021-05-17 12:30:08 -0600

    makes models

---
+++ models/List.js
@@ -0,0 +1,7 @@
+class List {
+  constructor(name, items) {
+    (this.name = name), (this.items = items);
+  }
+}
+
+export default List;
---
+++ models/ListItem.js
@@ -0,0 +1,10 @@
+class ListItem {
+  constructor(item, items, name, itemType) {
+    (this.item = item),
+      (this.items = items),
+      (this.name = name),
+      (this.itemType = itemType);
+  }
+}
+
+export default ListItem;
