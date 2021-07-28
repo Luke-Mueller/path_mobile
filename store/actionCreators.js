6a32e6a7796124e9ab5fe2f98078dfbf6df0ffad
Luke Mueller <lukemueller8@gmail.com>
2021-07-28 13:39:38 -0500

    removes action creator

--- store/actionCreators.js
+++ store/actionCreators.js
@@ -2,6 +2,7 @@ export const AUTH = "AUTH";
 export const LOGOUT = "LOGOUT";
 
 export const ACTIVATELIST = "ACTIVATELIST";
+export const ARCHIVELIST = "ARCHIVELIST";
 export const EDITLIST = "EDITLIST";
 export const GETLISTS = "GETLISTS";
 export const POSTLIST = "POSTLIST";
