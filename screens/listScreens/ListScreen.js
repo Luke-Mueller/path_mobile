b0e1d759bd1eea651295631ab3402c1c6e542700
Luke Mueller <lukemueller8@gmail.com>
2021-07-29 16:01:20 -0500

    removes archive btn from screen

--- screens/listScreens/ListScreen.js
+++ screens/listScreens/ListScreen.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import {
-  Button,
   FlatList,
   StyleSheet,
   Text,
@@ -9,30 +8,26 @@ import {
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { HeaderBackButton } from "@react-navigation/stack";
 import { Feather } from "@expo/vector-icons";
 
 import HeaderButton from "../../components/HeaderButton";
 import Color from "../../constants/color";
 import * as listsActions from "../../store/actions/lists";
 
-const ListScreen = (props) => {
-  const { navigation, route } = props;
-  const userId = useSelector((state) => state.auth.user._id);
-  const state = useSelector((state) => state);
+const ListScreen = ({ navigation, route }) => {
   const dispatch = useDispatch();
-
   const list = route.params.list;
+  const userId = useSelector((state) => state.auth.user._id);
 
   let bottomButtons = (
     <View style={styles.bottomContainer}>
-      <TouchableOpacity
+      {/* <TouchableOpacity
         style={styles.buttonContainer}
         onPress={() => archiveList()}
       >
         <Feather name="archive" size={24} color="black" />
         <Text style={{ marginHorizontal: 25 }}>Archive List</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
       <TouchableOpacity
         style={styles.buttonContainer}
         onPress={() => activateList()}
@@ -49,13 +44,16 @@ const ListScreen = (props) => {
         title="edit list"
         iconName="edit-2"
         IconComponent={Feather}
-        // onPress={() => navigation.navigate("Edit List", { listId: list._id })}
-        onPress={() => console.log("entering edit list")}
+        onPress={() => navigation.navigate("Edit List", { list })}
+        // onPress={() => console.log("entering edit list")}
       />
     </HeaderButtons>
   );
 
   useEffect(() => {
+    if (route.name === "Active List" || route.name === "Archived List") {
+      headerRight = null;
+    }
     navigation.setOptions({
       headerTitle: list.name,
       headerRight: headerRight,
@@ -65,20 +63,18 @@ const ListScreen = (props) => {
   const activateList = async () => {
     const payload = { list: list, userId: userId };
     dispatch(listsActions.activateList(payload, navigation));
+    navigation.pop();
   };
 
-  const archiveList = async () => {
-    const payload = { listId: list._id, userId };
-    dispatch(listsActions.archivelist(payload, navigation));
-  };
+  // const archiveList = async () => {
+  //   const payload = { listId: list._id, userId };
+  //   const done = dispatch(listsActions.archivelist(payload, navigation));
+  //   if(done) navigation.pop();
+  // };
 
-  // if (route.name === "Active List" || route.name === "Archived List") {
-  //   bottomButtons = null
-  //   // (
-  //   //   <Button title="SEE STATE" onPress={() => console.log("STATE: ", state)} />
-  //   // );
-  //   headerRight = null;
-  // }
+  if (route.name === "Active List" || route.name === "Archived List") {
+    bottomButtons = null;
+  }
 
   return (
     <View style={{ flex: 1 }}>
