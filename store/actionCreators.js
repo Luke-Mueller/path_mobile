d5e8595caf4c7dcf293f92b32e3a07c221097bc9
Luke Mueller <lukemueller8@gmail.com>
2021-08-05 12:45:44 -0500

    removes unused action creator

--- store/actionCreators.js
+++ store/actionCreators.js
@@ -1,8 +1,7 @@
 export const AUTH = "AUTH";
 export const LOGOUT = "LOGOUT";
 
-export const ACTIVATELIST = "ACTIVATELIST";
-export const ARCHIVELIST = "ARCHIVELIST";
+export const ADDLIST = "ARCHIVELIST";
 export const EDITLIST = "EDITLIST";
 export const GETLISTS = "GETLISTS";
 export const POSTLIST = "POSTLIST";
