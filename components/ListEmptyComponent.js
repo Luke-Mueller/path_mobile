9debed7955ae4f794f95ec33c14eef7b8b9aefda
Luke Mueller <lukemueller8@gmail.com>
2021-08-31 19:24:20 -0500

    changes listEmptyComponent content

--- components/ListEmptyComponent.js
+++ components/ListEmptyComponent.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const ListEmptyComponent = (props) => {
   let text = "You have no lists!";
   if (props.arc) text = "You have no archived lists!";
   if (props.sub) text = "Your sublist has no items!";
   return (
-    <View>
+    <View style={{flex: 1, alignItems: "center", paddingTop: '48%'}}>
+      <Image
+        style={{ height: 50, width: 50, marginBottom: 20 }}
+        source={require("../assets/box.png")}
+      />
       <Text>{text}</Text>
     </View>
   );
