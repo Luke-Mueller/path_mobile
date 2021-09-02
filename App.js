f4cb984cb092f70a57ce5b9265a28bed73cd8b86
Luke Mueller <lukemueller8@gmail.com>
2021-09-02 15:26:58 -0500

    adds paper themes

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
 import React from "react";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 import Thunk from "redux-thunk";
 
 import AppNavigator from "./navigation/AppNavigator";
 import AuthReducer from "./store/reducers/auth";
-import ListsReducer from './store/reducers/lists';
+import ListsReducer from "./store/reducers/lists";
 
 const rootReducer = combineReducers({
   auth: AuthReducer,
-  lists: ListsReducer
+  lists: ListsReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(Thunk));
+const theme = {
+  ...DefaultTheme,
+  roundness: 4,
+  // dark: true,
+  // mode: "adaptive",
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#00a8ff',
+  },
+};
 
 export default function App() {
   return (
     <Provider store={store}>
-      <AppNavigator />
+      <PaperProvider theme={theme}>
+        <AppNavigator />
+      </PaperProvider>
     </Provider>
   );
 }
