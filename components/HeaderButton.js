c5dce0cc43a24a78a7031444662aac009c3cc7b6
Luke Mueller <lukemueller8@gmail.com>
2021-09-28 13:19:11 -0500

    removes color default

--- components/HeaderButton.js
+++ components/HeaderButton.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { HeaderButton } from 'react-navigation-header-buttons';
-import { Feather } from '@expo/vector-icons';
-
-import Color from '../constants/color';
 
 const HeaderBtn = props => {
   return (
     <HeaderButton
       {...props}
       iconSize={23}
-      color={Color.black}
     />
   );
 };
